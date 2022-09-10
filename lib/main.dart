7a29427b3dadf6f603854f0dc6fd810447173f0d
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-09-10 21:24:59 +0500

    Background - Foreground

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
   }
 }
 
@@ -25,6 +28,19 @@ class HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple[100],
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              color: Colors.deepPurple,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
